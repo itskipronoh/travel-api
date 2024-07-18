06c2ecd1b1b0740dc6b38954750d358531ecf77c
Gideon-kipronoh <kipronogideon68@gmail.com>
2024-07-18 11:44:20 +0300

    Destination
    Signed-off-by: Gideon-kipronoh <kipronogideon68@gmail.com>

--- controllers/DestinationController.js
+++ controllers/DestinationController.js
@@ -9,6 +9,50 @@ const getAllDestinations = async (req, res) => {
     }
 };
 
+
+
+// Get a single destination by ID
+const getDestinationById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const destination = await Destination.findById(id);
+        if (!destination) {
+            return res.status(404).json({ message: 'Destination not found' });
+        }
+        res.status(200).json(destination);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Update a destination
+const updateDestination = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const destination = await Destination.findByIdAndUpdate(id, req.body, { new: true });
+        if (!destination) {
+            return res.status(404).json({ message: 'Destination not found' });
+        }
+        res.status(200).json(destination);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Delete a destination
+const deleteDestination = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const destination = await Destination.findByIdAndDelete(id);
+        if (!destination) {
+            return res.status(404).json({ message: 'Destination not found' });
+        }
+        res.status(200).json({ message: 'Destination deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
 const createDestination = async (req, res) => {
     try {
         const { name, description, location, imageUrl } = req.body;
@@ -19,4 +63,6 @@ const createDestination = async (req, res) => {
     }
 };
 
-module.exports = { getAllDestinations, createDestination };
+module.exports = { getAllDestinations, createDestination, getDestinationById,
+    updateDestination,
+    deleteDestination };
