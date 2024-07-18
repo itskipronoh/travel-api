23713e8c45e4da2aeac52fe1a5d3e063b4acb67c
Gideon-kipronoh <kipronogideon68@gmail.com>
2024-07-18 11:49:02 +0300

    destination routes
    Signed-off-by: Gideon-kipronoh <kipronogideon68@gmail.com>

--- routes/DestinationRoutes.js
+++ routes/DestinationRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const { getAllDestinations, createDestination } = require('../controllers/DestinationController');
+const { getAllDestinations, createDestination, getDestinationById,
+    updateDestination,
+    deleteDestination } = require('../controllers/DestinationController');
 const router = express.Router();
 
 router.get('/', getAllDestinations);
 router.post('/', createDestination);
+router.get('/:id', getDestinationById);
+router.put('/:id', updateDestination);
+router.delete('/:id', deleteDestination);
+
 
 module.exports = router;
